436d43a9914a3e96785d9f4d7fb66fef1e060830
Dmitriy Nevzorov <dmitriy.nevzorov@microapps.com>
2018-07-02 12:04:36 +0300

    - upgrade dependencies - extract api responses into apiUtils

--- src/hooks/attachUserPolicy.js
+++ src/hooks/attachUserPolicy.js
@@ -1,10 +1,6 @@
 const AWS = require('aws-sdk');
 const iot = new AWS.Iot();
-
-const headers = {
-  'Access-Control-Allow-Origin': '*', // Required for CORS support to work
-  'Access-Control-Allow-Credentials': true // Required for cookies, authorization headers with HTTPS
-};
+const {success, fail} = require('../lib/apiUtils');
 
 /**
  * Attaches iot policy to cognitoIdentityId.
@@ -20,26 +16,13 @@ module.exports.handler = async event => {
         policyName: process.env.USER_NOTIFICATIONS_POLICY
       })
       .promise();
-
-    return {
-      statusCode: 200,
-      headers,
-      body: 'Policy attached'
-    };
+    return success('Policy attached');
   } catch (error) {
     if (error.statusCode === 409) {
       // Policy already exists for this cognito identity
-      return {
-        statusCode: 200,
-        headers,
-        body: 'Policy already attached'
-      };
+      return success('Policy already attached');
     } else {
-      return {
-        statusCode: error.statusCode || 500,
-        headers,
-        body: JSON.stringify(error)
-      };
+      return fail(error);
     }
   }
 };
