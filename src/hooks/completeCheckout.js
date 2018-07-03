593ebe083c70e5a31ac670a3bda6f51529421764
Dmitriy Nevzorov <dmitriy.nevzorov@microapps.com>
2018-07-03 20:06:22 +0300

    - use transfer function directly as completeCheckout needs to wait until transaction is created

--- src/hooks/completeCheckout.js
+++ src/hooks/completeCheckout.js
@@ -4,10 +4,12 @@ const {getSecretValue} = require('../services/secrets');
 const {redirect} = require('../lib/apiUtils');
 const {isSuccessful} = require('../services/monei');
 const AWS = require('aws-sdk');
+const transferTokens = require('../steps/transferTokens').handler;
 
 const stepFunctions = new AWS.StepFunctions();
 
 module.exports.handler = async event => {
+  console.log(JSON.stringify(event, null, 2));
   const credentials = await getSecretValue(process.env.MONEI_CREDENTIALS_KEY);
   const {resourcePath} = event.queryStringParameters;
   const data = {
@@ -22,24 +24,24 @@ module.exports.handler = async event => {
   try {
     const res = await axios(options);
     console.log(JSON.stringify(res.data, null, 2));
+    const success = isSuccessful(res.data);
     const address = res.data.customer.merchantCustomerId;
     const amount = Number.parseFloat(res.data.amount) * 100;
+    if (success) {
+      const trx = await transferTokens({address, amount, note: 'Coins exchanged for EUR'});
 
-    if (isSuccessful(res.data)) {
-      // start transfer tokens state machine
+      console.log(JSON.stringify(trx, null, 2));
+      // start a CheckTransactionSM state machine to check transaction status
       await stepFunctions
         .startExecution({
-          stateMachineArn: process.env.TRANSFER_TOKENS_SM,
-          input: JSON.stringify({
-            address,
-            amount,
-            note: 'Coins exchanged for EUR'
-          })
+          stateMachineArn: process.env.CHECK_TRANSACTIN_SM,
+          input: JSON.stringify(trx)
         })
         .promise();
     }
     return redirect('http://localhost:4000');
   } catch (e) {
+    console.log(e);
     return redirect('http://localhost:4000');
   }
 };
