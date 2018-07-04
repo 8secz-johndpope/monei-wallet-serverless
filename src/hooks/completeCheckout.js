0414db68699a13fd34bb51d193e571a397ce5493
Dmitriy Nevzorov <dmitriy.nevzorov@microapps.com>
2018-07-04 16:56:57 +0300

    - create failed transaction if monei payment failes - properly handle errors in completeCheckout - save monei transaction description in note field

--- src/hooks/completeCheckout.js
+++ src/hooks/completeCheckout.js
@@ -1,12 +1,52 @@
 const axios = require('axios');
 const qs = require('qs');
 const {getSecretValue} = require('../services/secrets');
-const {redirect} = require('../lib/apiUtils');
-const {isSuccessful} = require('../services/monei');
+const {redirect, fail} = require('../lib/apiUtils');
+const {isSuccessful, getDescription} = require('../services/monei');
+const {withMasterAccount, masterAddress} = require('../services/etherium');
+const Transaction = require('../models/Transaction');
 const AWS = require('aws-sdk');
-const transferTokens = require('../steps/transferTokens').handler;
+const uniqid = require('uniqid');
 
 const stepFunctions = new AWS.StepFunctions();
+
+const transferTokens = async ({address, amount, note}) => {
+  const {token} = await withMasterAccount();
+
+  // create a transaction to transfer tokens
+  const tokenTransaction = token.methods.transfer(address, amount);
+
+  // estimate transaction gas
+  const gasNeeded = await tokenTransaction.estimateGas({
+    from: masterAddress,
+    gas: 60000
+  });
+
+  // send transaction and save it to dynamoDB
+  const trx = await new Promise((resolve, reject) => {
+    tokenTransaction.send({from: masterAddress, gas: gasNeeded}, (error, hash) => {
+      if (error) return reject(error);
+      Transaction.create({
+        id: hash,
+        from: masterAddress,
+        fromInfo: 'MONEI Coins exchange',
+        to: address,
+        amount,
+        note
+      }).then(resolve, reject);
+    });
+  });
+
+  // start a CheckTransactionSM state machine to check transaction status
+  await stepFunctions
+    .startExecution({
+      stateMachineArn: process.env.CHECK_TRANSACTIN_SM,
+      input: JSON.stringify(trx)
+    })
+    .promise();
+
+  return trx;
+};
 
 module.exports.handler = async event => {
   console.log(JSON.stringify(event, null, 2));
@@ -24,24 +64,34 @@ module.exports.handler = async event => {
   try {
     const res = await axios(options);
     console.log(JSON.stringify(res.data, null, 2));
-    const success = isSuccessful(res.data);
-    const address = res.data.customer.merchantCustomerId;
-    const amount = Number.parseFloat(res.data.amount) * 100;
-    if (success) {
-      const trx = await transferTokens({address, amount, note: 'Coins exchanged for EUR'});
 
-      console.log(JSON.stringify(trx, null, 2));
-      // start a CheckTransactionSM state machine to check transaction status
-      await stepFunctions
-        .startExecution({
-          stateMachineArn: process.env.CHECK_TRANSACTIN_SM,
-          input: JSON.stringify(trx)
-        })
-        .promise();
+    const address = res.data.customer.merchantCustomerId;
+    const amount = Number.parseFloat(res.data.amount || 0) * 100;
+    const note = getDescription(res.data);
+
+    if (isSuccessful(res.data)) {
+      await transferTokens({address, amount, note});
+    } else {
+      await Transaction.create({
+        id: uniqid(),
+        status: 'failed',
+        from: masterAddress,
+        fromInfo: 'MONEI Coins exchange',
+        to: address,
+        amount,
+        note
+      });
     }
+
     return redirect(process.env.FONTEND_URL);
-  } catch (e) {
-    console.log(e);
-    return redirect(process.env.FONTEND_URL);
+  } catch (error) {
+    if (error.response) {
+      return fail({
+        statusCode: error.response.status,
+        ...error.response.data
+      });
+    }
+    console.log(JSON.stringify(error, null, 2));
+    return fail(error);
   }
 };
--- src/services/monei.js
+++ src/services/monei.js
@@ -3,11 +3,19 @@ const qs = require('qs');
 
 const SUCCESS_RE = /^(000\.000\.|000\.100\.1|000\.[36]|000\.400\.0|000\.400\.100|000\.200)/;
 
-module.exports.isSuccessful = transactionResult => {
+exports.isSuccessful = transactionResult => {
   try {
     const resultCode = transactionResult.result.code;
     return SUCCESS_RE.test(resultCode);
   } catch (err) {
     return false;
+  }
+};
+
+exports.getDescription = transactionResult => {
+  try {
+    return transactionResult.result.description;
+  } catch (error) {
+    return '';
   }
 };
