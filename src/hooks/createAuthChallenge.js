4dc28f385d2998343aede464ade3e4b7b3374040
Dmitriy Nevzorov <dmitriy.nevzorov@microapps.com>
2018-07-10 17:48:37 +0300

    - refactor createAuthChallenge to use async/await - refactor verifyAuthChallenge to use async/await - add permissions to update user attributes for verifyAuthChallenge

--- src/hooks/createAuthChallenge.js
+++ src/hooks/createAuthChallenge.js
@@ -1,9 +1,11 @@
 const authy = require('authy')(process.env.AUTHY_API_KEY);
 const {PhoneNumberUtil} = require('google-libphonenumber');
+const {promisify} = require('es6-promisify');
 
 const phoneUtil = PhoneNumberUtil.getInstance();
+const verificationStart = promisify(authy.phones().verification_start);
 
-exports.handler = (event, context, callback) => {
+exports.handler = async event => {
   console.log(JSON.stringify(event, null, 2));
 
   // skip challenge if user has active session
@@ -21,9 +23,12 @@ exports.handler = (event, context, callback) => {
 
   event.response.challengeMetadata = 'PHONE_VERIFICATION_CHALLENGE';
 
-  authy.phones().verification_start(nationalNumber, countryCode, 'sms', (err, res) => {
-    if (err) return callback(err.message);
-    if (res.success) return callback(null, event);
-    return callback('Something went wrong.');
-  });
+  try {
+    const res = await verificationStart(nationalNumber, countryCode, 'sms');
+    if (res.success) return event;
+    return res.message;
+  } catch (error) {
+    console.log(JSON.stringify(error, null, 2));
+    return error.message || 'Something went wrong';
+  }
 };
