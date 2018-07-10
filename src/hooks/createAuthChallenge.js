9a6f7e3c150f91fc415e77bedabf618b920bd159
Dmitriy Nevzorov <dmitriy.nevzorov@microapps.com>
2018-07-10 13:30:20 +0300

    - phone verification start logic - phone verification check logic - add authy api key to travis

--- src/hooks/createAuthChallenge.js
+++ src/hooks/createAuthChallenge.js
@@ -1,12 +1,29 @@
-module.exports.handler = async event => {
-  if (!event.request.session || event.request.session.length === 0) {
-    event.response.publicChallengeParameters = {
-      captchaUrl: 'url/123.jpg'
-    };
-    event.response.privateChallengeParameters = {
-      answer: '5'
-    };
-    event.response.challengeMetadata = 'CAPTCHA_CHALLENGE';
-  }
-  return event;
+const authy = require('authy')(process.env.AUTHY_API_KEY);
+const {PhoneNumberUtil} = require('google-libphonenumber');
+
+const phoneUtil = PhoneNumberUtil.getInstance();
+
+module.exports.handler = (event, context, callback) => {
+  console.log(JSON.stringify(event, null, 2));
+
+  // skip challenge if user has active session
+  if (event.request.session && event.request.session.length > 0) return callback(null, event);
+
+  const phoneNumber = phoneUtil.parse(event.request.userAttributes.phone_number);
+  const nationalNumber = phoneNumber.getNationalNumber();
+  const countryCode = phoneNumber.getCountryCode();
+
+  // pass formatted phone to the verifyAuthChallenge hook
+  event.response.privateChallengeParameters = {
+    nationalNumber,
+    countryCode
+  };
+
+  event.response.challengeMetadata = 'PHONE_VERIFICATION_CHALLENGE';
+
+  authy.phones().verification_start(nationalNumber, countryCode, 'sms', (err, res) => {
+    if (err) return callback(err.message);
+    if (res.success) return callback(null, event);
+    return callback('Something went wrong.');
+  });
 };
--- src/hooks/verifyAuthChallenge.js
+++ src/hooks/verifyAuthChallenge.js
@@ -1,9 +1,15 @@
-module.exports.handler = async (event, context) => {
-  if (event.request.privateChallengeParameters.answer === event.request.challengeAnswer) {
-    event.response.answerCorrect = true;
-  } else {
-    event.response.answerCorrect = false;
-  }
+const authy = require('authy')(process.env.AUTHY_API_KEY);
+
+module.exports.handler = (event, context, callback) => {
+  const {nationalNumber, countryCode} = event.request.privateChallengeParameters;
+
+  authy
+    .phones()
+    .verification_check(nationalNumber, countryCode, event.request.challengeAnswer, (err, res) => {
+      if (err) return callback(err.message);
+      event.response.answerCorrect = res.success;
+      return callback(null, event);
+    });
 
   return event;
 };
