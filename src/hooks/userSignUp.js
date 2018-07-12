009de57578fc638bc099776b0701b7105d3a59e6
Dmitriy Nevzorov <dmitriy.nevzorov@microapps.com>
2018-07-12 16:02:32 +0300

    fix api response for userSignUp

--- src/hooks/userSignUp.js
+++ src/hooks/userSignUp.js
@@ -1,16 +1,26 @@
 const AWS = require('aws-sdk');
 const PasswordGenerator = require('strict-password-generator').default;
+const {success, fail} = require('../lib/apiUtils');
 
 const provider = new AWS.CognitoIdentityServiceProvider();
 const passwordGenerator = new PasswordGenerator();
 
 exports.handler = async event => {
   console.log(JSON.stringify(event, null, 2));
+  const {username} = JSON.parse(event.body);
 
   const params = {
     ClientId: process.env.USER_POOL_CLIENT_ID,
     Password: passwordGenerator.generatePassword(),
-    Username: event.body.username
+    Username: username
   };
-  return provider.signUp(params).promise();
+
+  try {
+    const user = await provider.signUp(params).promise();
+    console.log(JSON.stringify(user, null, 2));
+    return success(user);
+  } catch (error) {
+    console.log(JSON.stringify(error, null, 2));
+    return fail(error);
+  }
 };
