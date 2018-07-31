a8fa3cc44b99dad7d329c9ebddacbe034a055fbb
Dmitriy Nevzorov <dmitriy.nevzorov@microapps.com>
2018-07-31 18:30:47 +0300

    attachBankAccount endpoint

--- src/resolvers/attachBankAccount.js
+++ src/resolvers/attachBankAccount.js
@@ -1,4 +1,12 @@
+const TransferWise = require('../services/transferwise');
+const {getSecretValue} = require('../services/secrets');
+
 // creates transferwise account
 exports.handler = async event => {
   const {accountHolderName, country, IBAN} = event.arguments;
+  const creds = await getSecretValue(process.env.TRANSFERWISE_CREDENTIALS_KEY);
+  const options = JSON.parse(creds);
+  options.profile = 256;
+  const client = new TransferWise(options);
+  return client.createAccount({accountHolderName, country, IBAN});
 };
