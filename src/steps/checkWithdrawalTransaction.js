dc8eb9b8790dc2c3837cd7c51c5b7a185b48137d
Dmitriy Nevzorov <dmitriy.nevzorov@microapps.com>
2018-09-26 13:43:32 +0300

    change withdraw-tokens-sm logic to update transactions as soon as it is mined but wait for 10 confirmations to transfer fiat

--- src/steps/checkWithdrawalTransaction.js
+++ src/steps/checkWithdrawalTransaction.js
@@ -1,19 +1,24 @@
 const {web3} = require('../services/etherium');
 const Transaction = require('../models/Transaction');
 
-exports.handler = async ({transaction, bankAccountId}) => {
+exports.handler = async ({transaction, bankAccountId, confirmations = 0}) => {
   const [currentBlock, receipt] = await Promise.all([
     web3.eth.getBlockNumber(),
     web3.eth.getTransactionReceipt(transaction.id)
   ]);
 
-  // exit if no receipt (transaction is not mined yet) or has less then 10 blocks ahead
-  if (!receipt || currentBlock - receipt.blockNumber < 10) {
-    return {transaction, bankAccountId};
+  // exit if no receipt (transaction is not mined yet)
+  if (!receipt) {
+    return {transaction, bankAccountId, confirmations};
   }
 
-  // updates dynamoDB record
-  const trx = await Transaction.updateFromReceipt(receipt);
+  // update transaction if receipt exists and transaction status is pending
+  if (transaction.status === 'pending') {
+    transaction = await Transaction.updateFromReceipt(receipt);
+  }
 
-  return {transaction: trx, bankAccountId};
+  // update confirmations counter
+  confirmations = currentBlock - receipt.blockNumber;
+
+  return {transaction, bankAccountId, confirmations};
 };
--- src/steps/transferFiat.js
+++ src/steps/transferFiat.js
@@ -4,6 +4,24 @@ const Cognito = require('../services/cognito');
 
 const cognito = new Cognito();
 
+/**
+{
+  "transaction": {
+    "createdAt": 1537956636051,
+    "updatedAt": 1537956636051,
+    "status": "pending",
+    "id": "0x91d34fc00971be420b2522e82edc865abb00a70140d9eb0a222331fddefdb480",
+    "from": "0xaA8bdEe6C688c2dA8c3A15397c20CD116e482e73",
+    "fromInfo": "0xaA8bdEe6C688c2dA8c3A15397c20CD116e482e73",
+    "to": "0x4af669e4f4422c0d867415ab0784005bf989a992",
+    "toInfo": "MONEI Coins exchange",
+    "amount": 100,
+    "note": "Transferred to your bank account. You'll receive funds within 24 hours"
+  },
+  "bankAccountId": "14181225"
+}
+ */
+
 exports.handler = async data => {
   JSON.stringify(data, null, 2);
 
